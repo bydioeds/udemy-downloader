b30225b1b894e659b3e5df2b031e460d340b856f
pansutodeus <pansutodeus@debian>
2023-05-16 14:02:45 -0600

    User-Agent Updated

--- constants.py
+++ constants.py
@@ -4,8 +4,7 @@ import time
 
 HEADERS = {
     "Origin": "www.udemy.com",
-    # "User-Agent":
-    # "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
     "Accept": "*/*",
     "Accept-Encoding": None,
 }
